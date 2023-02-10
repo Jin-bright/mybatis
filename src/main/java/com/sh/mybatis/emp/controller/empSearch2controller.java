a756ae56eaff226a6b0855adf97f49aad4df0ae0
J <77500290+Jin-bright@users.noreply.github.com>
2023-02-10 15:44:25 +0900

    Update empSearch2controller.java

--- src/main/java/com/sh/mybatis/emp/controller/empSearch2controller.java
+++ src/main/java/com/sh/mybatis/emp/controller/empSearch2controller.java
@@ -1,8 +1,6 @@
 package com.sh.mybatis.emp.controller;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
